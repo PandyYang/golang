2e7484d5091ed691dd0663d8f8ec0fb42b20d695
Pandy <30763792+PandyYang@users.noreply.github.com>
2019-05-26 14:40:25 +0800

    string/rune

---
+++ go/container/string.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main(){
+	s := "PandyYang"//UTF-8 编码
+	fmt.Printf("%X\n",[]byte(s))
+	for _, b := range []byte(s){
+		fmt.Printf("%X ",b)
+	}
+	fmt.Println()
+	for i,ch := range s {
+		fmt.Printf("(%d %X) ",i, ch)
+	}
+	fmt.Println()
+	fmt.Println("Rune Count:",utf8.RuneCountInString(s))//获得字符数量
+
+	bytes := []byte(s)
+	for len(bytes) > 0 {
+		ch, size := utf8.DecodeRune(bytes)
+		bytes = bytes[size:]
+		fmt.Printf("%c ",ch)
+	}
+	fmt.Println()
+
+	for i, ch := range []rune(s){
+		fmt.Printf("(%d %c)",i, ch)
+	}
+	fmt.Println()
+
+
+
+}
