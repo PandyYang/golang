338419f07fa179a9828073fbc1d0df7a0a4239a9
PandyYang <yangpandy@gmail.com>
2022-05-17 19:29:51 +0800

    通道的遍历

---
+++ channel_iter.go
@@ -0,0 +1,38 @@
+package main
+
+var c = make(chan int)
+
+//func main() {
+//
+//	go func() {
+//		for i := 0; i < 2; i++ {
+//			c <- i
+//		}
+//
+//		// 如果没有关闭通道 会造成死锁
+//		close(c)
+//	}()
+//
+//	// for range的形式遍历通道
+//	//for i2 := range c {
+//	//	fmt.Printf("r: %v\n", i2)
+//	//}
+//
+//	for {
+//		v, ok := <-c
+//		if ok {
+//			fmt.Printf("r: %v\n", v)
+//		} else {
+//			break
+//		}
+//	}
+//
+//	//r := <-c
+//	//fmt.Printf("r: %v\n", r)
+//	//
+//	//r = <-c
+//	//fmt.Printf("r: %v\n", r)
+//	//
+//	//r = <-c
+//	//fmt.Printf("r: %v\n", r)
+//}
