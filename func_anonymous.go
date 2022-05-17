1f1f8c878935ac64adcd800176ce5e4839f8f920
PandyYang <yangpandy@gmail.com>
2022-05-17 12:22:23 +0800

    ano func run self

--- func_anonymous.go
+++ func_anonymous.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	// anonymous function
 	max := func(a int, b int) int {
 		if a > b {
 			return a
@@ -14,4 +16,15 @@ func main() {
 	i := max(1, 2)
 
 	fmt.Print(i)
+
+	// run self
+	func(a int, b int) {
+		max := 0
+		if a > b {
+			max = a
+		} else {
+			max = b
+		}
+		fmt.Printf("max is %v\n", max)
+	}(1, 2)
 }
