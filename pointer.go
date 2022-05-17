712d3295d603ba4f42e4c4090732e9197b81f06b
PandyYang <yangpandy@gmail.com>
2022-05-17 12:52:04 +0800

    pointer

---
+++ pointer.go
@@ -0,0 +1,20 @@
+package main
+
+//func main() {
+//	var ip *int
+//
+//	fmt.Printf("ip: %v\n", ip)
+//	fmt.Printf("ip: %T\n", ip)
+//
+//	var i int = 100
+//	ip = &i
+//	fmt.Printf("ip: %v\n", ip)
+//	fmt.Printf("ip: %v\n", *ip)
+//	fmt.Printf("ip: %v\n", &ip)
+//
+//	var sp *string
+//	var s string = "hello"
+//
+//	sp = &s
+//	fmt.Printf("sp: %v\n", sp)
+//}
