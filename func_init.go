c4799c88791e64dec3749073063f622eab5372d5
PandyYang <yangpandy@gmail.com>
2022-05-17 12:32:47 +0800

    init func

--- func_init.go
+++ func_init.go
@@ -1,12 +1,10 @@
 package main
 
-import "fmt"
-
 // init is a special function, always run ahead main func
 func init() {
 	println("init...")
 }
 
-func main() {
-	fmt.Println("main...")
-}
+//func main() {
+//	fmt.Println("main...")
+//}
