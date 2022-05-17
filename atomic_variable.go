de828d9118e8afb7b29a072213426fc2913fbac5
PandyYang <yangpandy@gmail.com>
2022-05-17 20:35:31 +0800

    原子操作

---
+++ atomic_variable.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"sync/atomic"
+)
+
+var i int32 = 100
+
+func add() {
+	atomic.AddInt32(&i, 1)
+}
+
+func sub() {
+	atomic.AddInt32(&i, -1)
+}
+
+//func main() {
+//	for i := 0; i < 100; i++ {
+//		go add()
+//		go sub()
+//	}
+//
+//	time.Sleep(time.Second * 2)
+//	fmt.Printf("end is: %v\n", i)
+//}
