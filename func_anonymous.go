d14afce823e266a2422a47a12044fb9ffb40f14f
PandyYang <yangpandy@gmail.com>
2022-05-17 12:29:41 +0800

    闭包

--- func_anonymous.go
+++ func_anonymous.go
@@ -1,30 +1,28 @@
 package main
 
-import "fmt"
-
-func main() {
-
-	// anonymous function
-	max := func(a int, b int) int {
-		if a > b {
-			return a
-		} else {
-			return b
-		}
-	}
-
-	i := max(1, 2)
-
-	fmt.Print(i)
-
-	// run self
-	func(a int, b int) {
-		max := 0
-		if a > b {
-			max = a
-		} else {
-			max = b
-		}
-		fmt.Printf("max is %v\n", max)
-	}(1, 2)
-}
+//func main() {
+//
+//	// anonymous function
+//	max := func(a int, b int) int {
+//		if a > b {
+//			return a
+//		} else {
+//			return b
+//		}
+//	}
+//
+//	i := max(1, 2)
+//
+//	fmt.Print(i)
+//
+//	// run self
+//	func(a int, b int) {
+//		max := 0
+//		if a > b {
+//			max = a
+//		} else {
+//			max = b
+//		}
+//		fmt.Printf("max is %v\n", max)
+//	}(1, 2)
+//}
---
+++ func_closure.go
@@ -0,0 +1,17 @@
+package main
+
+// 闭包是指一个函数和与其相关的引用环境而组合的实体
+// 闭包 = 函数 + 引用环境
+func add() func(int) int {
+	var x int
+	return func(y int) int {
+		x += y
+		return x
+	}
+}
+
+//func main() {
+//	var f = add()
+//	println(f(10))
+//	println(f(10))
+//}
